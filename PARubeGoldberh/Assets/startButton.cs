89338b75afaab09cde84f69bb0d9cb9cff9f4d37
kyawai <37072499+kyawai@users.noreply.github.com>
2018-05-16 18:50:15 +0100

    Scences

---
+++ PARubeGoldberh/Assets/startButton.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class startButton : MonoBehaviour {
+
+    public void changemenu ( string play)
+    {
+        Application.LoadLevel("play");
+    }
+}
